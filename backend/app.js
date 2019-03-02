f680f6bb85390a83da876465230e006a5322ed4b
Israel Garcia <israel_gahe@hotmail.com>
2019-03-02 17:01:08 -0600

    vamoa ver el codigo de dany

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,15 @@ const socketIO = require('socket.io');
 
 let players={};
 
+let map=[
+    {x:600,y:0,w:100,h:200},
+    {x:0,y:300,w:100,h:100},
+    {x:200,y:300,w:500,h:100},
+    {x:0,y:600,w:300,h:100},
+    {x:400,y:500,w:100,h:500},
+    {x:600,y:600,w:400,h:100}
+]
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -19,7 +28,6 @@ app.all('/*', function(req, res, next) {
 });
 
 app.get('/players',function(req,res){
-    console.log('pidieron los players',players.length);
     res.header('Content-Type', 'application/json');
     res.send(JSON.stringify(players));
 })
@@ -28,13 +36,14 @@ io.on('connection', socket => {
     console.log('new connection',players)
 
     socket.on('setAlias',(alias)=>{
+        console.log('set Alias',alias);
         if(players[alias]==undefined){
-            players[alias]={alias:alias,x:0,y:0,alive:true};
+            players[alias]={alias:alias,x:10,y:20,alive:true};
 
-            let game={'map':[],'me':players[alias]};
+            let game={'map':map,'me':players[alias]};
             socket.emit('gameStart',game);
             console.log(players);
-        } 
+        }
     });
 
     socket.on('move',(coord)=>{
